5e6ccc5d605301198fd19c9e9907993d3cd45af0
adamwinzdesign <adamwinzdesign@gmail.com>
2019-10-13 10:56:14 -0500

    resolved issue with passing props

--- src/components/quoteMain.js
+++ src/components/quoteMain.js
@@ -10,7 +10,7 @@ const QuoteMain = () => {
   useEffect(() => {
     axios.get('https://thesimpsonsquoteapi.glitch.me/quotes?count=3')
       .then(response => {
-        console.log(response.data[0]);
+        console.log(response); // one object, data: Array, each element in array is an object
         setQuotes(response.data);
       })
       .catch(error => {
@@ -23,8 +23,8 @@ const QuoteMain = () => {
     <div className = 'quoteMain-contain'>
       <h1>Test</h1>
 
-      {quotes.map((quote, index) => {
-        return <QuoteCard key = {index} quote = {quote} />
+      {quotes.map((quote) => {
+        return <QuoteCard key = {quote.index} character = {quote.character} quote = {quote.quote} />
       })}
     </div>
   )
