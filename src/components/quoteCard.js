f5ff4bb1caffe68a752660c168a67722270e31bb
adamwinzdesign <adamwinzdesign@gmail.com>
2019-10-13 22:32:23 -0500

    styling

--- src/components/quoteCard.js
+++ src/components/quoteCard.js
@@ -9,7 +9,7 @@ const QuoteCard = (props) => {
   return (
     <div className = 'card-contain' key = '{props.key}'>
       <Card body inverse color="dark">
-        <CardBody>
+        <CardBody className = 'cardBody'>
           <CardTitle className = 'title'>Character: {props.character}</CardTitle>
           <CardText className = 'quoteText'>Character Quote: "{props.quote}"</CardText>
           <img src = {props.image} alt = 'quoted character'/>
